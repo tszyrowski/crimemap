4ac8aa3d800c7dfb73eb482ca91db9786d3b06c5
Tomasz <tszyrowski@gmail.com>
2017-09-05 14:00:25 +0100

    last changes with validation

--- crimemap.py
+++ crimemap.py
@@ -4,8 +4,13 @@ Created on 18 Aug 2017
 @author: T
 '''
 import json
+import datetime
+import dateparser
+import string
 
 import dbconfig
+import dateparser
+
 if dbconfig.test:
     from mockdbhelper import MockDBHelper as DBHelper
 else:
@@ -19,11 +24,14 @@ from flask import request
 app = Flask(__name__)
 DB = DBHelper()
 
+categories = ['mugging', 'break-in']
+
 @app.route("/")
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home.html", crimes=crimes)
+    return render_template("home.html", crimes=crimes, 
+                           categories=categories, error_message=error_message)
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -45,12 +53,31 @@ def clear():
 @app.route("/submitcrime", methods=['POST'])  
 def submitcrimme():
     category = request.form.get("category")
-    date = request.form.get("date")
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
-    description = request.form.get("description")
+    if category not in categories:
+        return home()
+    date = format_date(request.form.get("date"))
+    if not date:
+        return home("Invalid date. Please use yyyy-mm-dd format")
+    try:
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+    except ValueError:
+        return home()
+    description = sanitize_string(request.form.get("description"))
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+def sanitize_string(userinput):
+    whitelist = string.ascii_letters + string.digits + "!?$.,:;-'()&"
+    return filter(lambda x: x in whitelist, userinput)
+    
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
---
+++ src/sideTries.py
@@ -0,0 +1,11 @@
+'''
+Created on 5 Sep 2017
+
+@author: T
+
+Script to try different options, trips and tricks
+'''
+import dateparser
+print(dateparser.parse("1-jan/15"))
+print(dateparser.parse("1 week and 3 days ago"))
+print(dateparser.parse("3/4/17"))
